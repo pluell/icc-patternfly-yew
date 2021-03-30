678cce9f526d1d4d26b6e7e854ae39daf284b582
Patrick Luell <pluell@iccdesigns.com>
2021-03-30 11:55:35 -0500

    Constants: Add key codes

--- icc-patternfly-yew-core/src/helpers/constants.rs
+++ icc-patternfly-yew-core/src/helpers/constants.rs
@@ -7,6 +7,16 @@ pub const BREAKPOINT_LG: i32 = 992;
 pub const BREAKPOINT_XL: i32 = 1200;
 pub const BREAKPOINT_XXL: i32 = 1450;
 
+pub enum KeyCodes
+{ 
+    Tab = 9,
+    Enter = 13,
+    EscapeKey = 27,
+    Space = 32,
+    ArrowUp = 38,
+    ArrowDown = 40,
+}
+
 #[derive(Clone, PartialEq)]
 pub enum ValidatedOptions
 {
