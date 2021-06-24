e60a5efe59a228a7cb7a25b45008767825329434
Patrick Luell <pluell@iccdesigns.com>
2021-06-24 14:19:26 -0500

    icc-patternfly-yew-icons: add angle icons

--- icc-patternfly-yew-icons/src/lib.rs
+++ icc-patternfly-yew-icons/src/lib.rs
@@ -2,6 +2,28 @@
 
 
 #[macro_export]
+macro_rules! angle_left_icon {
+    () => {
+        html!{
+            <svg style="vertical-align:-0.125em" fill="currentColor" height="1em" width="1em" viewBox="0 0 256 512" aria-hidden="true" role="img">
+                <path d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z" />
+            </svg>
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! angle_right_icon {
+    () => {
+        html!{
+            <svg style="vertical-align:-0.125em" fill="currentColor" height="1em" width="1em" viewBox="0 0 256 512" aria-hidden="true" role="img">
+                <path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z" />
+            </svg>
+        }
+    }
+}
+
+#[macro_export]
 macro_rules! bell_icon {
     () => {
         html!{
