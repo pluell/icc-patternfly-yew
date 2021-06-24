d5f5bcda3c6b20aeb29440388c37634ce6e99c5c
Patrick Luell <pluell@iccdesigns.com>
2021-06-24 14:40:15 -0500

    Util: add is element in view function

--- icc-patternfly-yew-core/src/helpers/util.rs
+++ icc-patternfly-yew-core/src/helpers/util.rs
@@ -1,5 +1,6 @@
 
 use rand::{distributions::Alphanumeric, Rng};
+use web_sys::{Element};
 
 
 pub fn utils_get_unique_id(prefix: Option<String>) -> String
@@ -15,3 +16,35 @@ pub fn utils_get_unique_id(prefix: Option<String>) -> String
 
     format!("{}-{}", pf, uid)
 }
+
+/** This function returns whether or not an element is within the viewable area of a container. If partial is true,
+ * then this function will return true even if only part of the element is in view.
+ *
+ * @param {Element} container  The container to check if the element is in view of.
+ * @param {Element} element    The element to check if it is view
+ * @param {boolean} partial   true if partial view is allowed
+ *
+ * @returns { boolean } True if the component is in View.
+ */
+pub fn is_element_in_view(container: &Element, element: &Element, partial: bool) -> bool
+{
+    // Get bounding rectangles
+    let container_bounds = container.get_bounding_client_rect();
+    let element_bounds = element.get_bounding_client_rect();
+
+    // Get side bounds
+    let container_bounds_left = container_bounds.left().floor();
+    let container_bounds_right = container_bounds.right().floor();
+    let element_bounds_left = element_bounds.left().floor();
+    let element_bounds_right = element_bounds.right().floor();
+  
+    // Check if in view
+    let is_totally_in_view = element_bounds_left >= container_bounds_left && element_bounds_right <= container_bounds_right;
+    let is_partially_in_view =
+      partial &&
+      ((element_bounds_left < container_bounds_left && element_bounds_right > container_bounds_left) ||
+        (element_bounds_right > container_bounds_right && element_bounds_left < container_bounds_right));
+
+    // Return outcome
+    is_totally_in_view || is_partially_in_view
+  }
