8b61fbe3c3a5602a744f806ca2b85cca810e6f53
Patrick Luell <pluell@iccdesigns.com>
2024-09-04 09:57:13 -0500

    ChipGroup: implement more props
    Implement overflow chip view

--- icc-patternfly-yew-core/src/components/chip_group/chip.rs
+++ icc-patternfly-yew-core/src/components/chip_group/chip.rs
@@ -1,5 +1,7 @@
 use yew::prelude::*;
 
+use icc_patternfly_yew_icons::times_icon;
+
 use crate::components::{Button, ButtonVariant};
 
 
@@ -8,11 +10,52 @@ pub struct Chip;
 #[derive(Clone, PartialEq, Properties)]
 pub struct ChipProperties
 {
+    /** Badge to add to the chip. The badge will be rendered after the chip text. */
+    #[prop_or_default]
+    pub badge: Option<Html>,
+    /** Content rendered inside the chip text */
     pub children: Children,
+    /** Aria Label for close button */
+    #[prop_or_default]
+    pub close_btn_aria_label: Option<String>,
+    /** Additional classes added to the chip item */
+    #[prop_or_default]
+    pub classes: Classes,
+    /** Flag indicating if the chip is an overflow chip */
+    #[prop_or_default]
+    pub is_overflow_chip: bool,
+    /** Flag indicating if chip is read only */
     #[prop_or_default]
     pub is_read_only: bool,
+    /** Function that is called when clicking on the chip close button */
     #[prop_or_default]
     pub onclick: Callback<MouseEvent>,
+    /** Component that will be used for chip. It is recommended that <button> or <li>  are used when the chip is an overflow chip. */
+    #[prop_or(String::from("div"))]
+    pub component: String,
+    // /** Position of the tooltip which is displayed if text is longer */
+    // tooltipPosition?:
+    //     | TooltipPosition
+    //     | 'auto'
+    //     | 'top'
+    //     | 'bottom'
+    //     | 'left'
+    //     | 'right'
+    //     | 'top-start'
+    //     | 'top-end'
+    //     | 'bottom-start'
+    //     | 'bottom-end'
+    //     | 'left-start'
+    //     | 'left-end'
+    //     | 'right-start'
+    //     | 'right-end';
+
+    /** Css property expressed in percentage or any css unit that overrides the default value of the max-width of the chip's text */
+    #[prop_or_default]
+    pub text_max_width: Option<String>,
+    /** Value to overwrite the randomly generated data-ouia-component-id.*/
+    #[prop_or_default]
+    pub ouia_id: Option<String>,
 }
 
 impl Component for Chip
@@ -27,23 +70,62 @@ impl Component for Chip
 
     fn view(&self, ctx: &Context<Self>) -> Html
     {
+        if ctx.props().is_overflow_chip {
+            self.view_overflow_chip(ctx)
+        } else {
+            self.view_chip(ctx)
+        }
+    }
+}
+
+impl Chip
+{
+    fn view_chip(&self, ctx: &Context<Self>) -> Html
+    {
+        let style = if let Some(text_max_width) = &ctx.props().text_max_width {
+            Some(format!("max-width: {}", text_max_width))
+        } else {
+            None
+        };
+
         html!{
-            <div
-                class={"pf-v5-c-chip"}
+            <@{ctx.props().component.clone()}
+                {style}
+                class={classes!("pf-v5-c-chip", ctx.props().classes.clone())}
+                // {...(this.state.isTooltipVisible && { tabIndex: 0 })}
+                // {...getOUIAProps(Chip.displayName, ouiaId !== undefined ? ouiaId : this.state.ouiaStateId)}
+                // {...props}
             >
-                <span class={"pf-v5-c-chip__text"}>
-                    {ctx.props().children.clone()}
+                <span class={"pf-v5-c-chip__content"}>
+                    <span class={"pf-v5-c-chip__text"}>
+                        {ctx.props().children.clone()}
+                    </span>
+                    {
+                        if let Some(badge) = &ctx.props().badge
+                        {
+                            badge.clone()
+                        }
+                        else{
+                            html!{}
+                        }
+                    }
                 </span>
                 {
                     if !ctx.props().is_read_only
                     {
                         html!{
-                            <Button
-                                variant={ButtonVariant::Plain}
-                                onclick={ctx.props().onclick.clone()}
-                            >
-                                <i class={"fas fa-times"}></i>
-                            </Button>
+                            <span class="pf-v5-c-chip__actions">
+                                <Button
+                                    variant={ButtonVariant::Plain}
+                                    onclick={ctx.props().onclick.clone()}
+                                    aria_label={ctx.props().close_btn_aria_label.clone()}
+                                    // id={`remove_${id}`}
+                                    // aria-labelledby={`remove_${id} ${id}`}
+                                    // ouiaId={ouiaId || closeBtnAriaLabel}
+                                >
+                                    {times_icon!{}}
+                                </Button>
+                            </span>
                         }
                     }
                     else
@@ -51,7 +133,46 @@ impl Component for Chip
                         html!{}
                     }
                 }
-            </div>
+            </@>
+        }
+    }
+
+    fn view_overflow_chip(&self, ctx: &Context<Self>) -> Html
+    {
+        let style = if let Some(text_max_width) = &ctx.props().text_max_width {
+            Some(format!("max-width: {}", text_max_width))
+        } else {
+            None
+        };
+
+        let type_prop = if ctx.props().component == "button" {    
+            Some(String::from("button"))
+        } else {
+            None
+        };
+
+        html!{
+            <@{ctx.props().component.clone()}
+                onclick={ctx.props().onclick.clone()}
+                {style}
+                class={classes!("pf-v5-c-chip", "pf-m-overflow", ctx.props().classes.clone())}
+                type={type_prop}
+                // {...getOUIAProps('OverflowChip', ouiaId !== undefined ? ouiaId : this.state.ouiaStateId)}
+                // {...props}
+            >
+                <span class={"pf-v5-c-chip__content"}>
+                    <span class={"pf-v5-c-chip__text"}>
+                        {ctx.props().children.clone()}
+                    </span>
+                    {
+                        if let Some(badge) = &ctx.props().badge {
+                            badge.clone()
+                        } else {
+                            html!{}
+                        }
+                    }
+                </span>
+            </@>
         }
     }
 }
--- icc-patternfly-yew-core/src/components/chip_group/chip_group.rs
+++ icc-patternfly-yew-core/src/components/chip_group/chip_group.rs
@@ -1,28 +1,76 @@
 use yew::prelude::*;
 
-use crate::components::{Button, ButtonVariant};
+use crate::components::{Button, ButtonVariant, Chip};
 
 
-pub struct ChipGroup;
+pub struct ChipGroup
+{
+    is_open: bool,
+}
 
 pub enum ChipGroupMsg
 {
+    OnClickOverflowChip,
 }
 
 #[derive(Clone, PartialEq, Properties)]
 pub struct ChipGroupProperties
 {
+    /** Content rendered inside the chip group. Should be <Chip> elements. */
+    pub children: Children,
+    /** Additional classes added to the chip item */
+    #[prop_or_default]
+    pub classes: Classes,
+    /** Flag for having the chip group default to expanded */
     #[prop_or_default]
     pub default_is_open: bool,
+    /** Customizable "Show Less" text string */
+    #[prop_or(Some(String::from("Show Less")))]
+    pub expanded_text: Option<String>,
+    /** Customizeable template string. Use variable "${remaining}" for the overflow chip count. */
+    #[prop_or_default]
+    pub collapsed_text: Option<String>,
+    /** Category name text for the chip group category.  If this prop is supplied the chip group with have a label and category styling applied */
     #[prop_or_default]
     pub category_name: String,
+    /** Aria label for chip group that does not have a category name */
     #[prop_or_default]
+    pub aria_label: Option<String>,
+    /** Set number of chips to show before overflow */
+    #[prop_or(3)]
     pub num_chips: i32,
+    /** Flag if chip group can be closed*/
     #[prop_or_default]
     pub is_closable: bool,
+    /** Aria label for close button */
+    #[prop_or_default]
+    pub close_btn_aria_label: Option<String>,
+    /** Function that is called when clicking on the chip group close button */
     #[prop_or_default]
     pub onclick: Callback<MouseEvent>,
-    pub children: Children,
+    /** Function that is called when clicking on the overflow (expand/collapse) chip button */
+    #[prop_or_default]
+    pub on_overflow_chip_click: Callback<()>, //(event: React.MouseEvent) => void;
+    // /** Position of the tooltip which is displayed if the category name text is longer */
+    // #[prop_or_default]
+    // tooltipPosition?:
+    //     | TooltipPosition
+    //     | 'auto'
+    //     | 'top'
+    //     | 'bottom'
+    //     | 'left'
+    //     | 'right'
+    //     | 'top-start'
+    //     | 'top-end'
+    //     | 'bottom-start'
+    //     | 'bottom-end'
+    //     | 'left-start'
+    //     | 'left-end'
+    //     | 'right-start'
+    //     | 'right-end';
+    /** Value to overwrite the randomly generated data-ouia-component-id.*/
+    #[prop_or_default]
+    pub ouia_id: Option<String>,
 }
 
 impl Component for ChipGroup
@@ -32,7 +80,23 @@ impl Component for ChipGroup
 
     fn create(_: &Context<Self>) -> Self
     {
-        Self
+        Self {
+            is_open: false,
+        }
+    }
+
+    fn update(&mut self, ctx: &Context<Self>, msg : Self::Message) -> bool
+    {
+        match msg
+        {
+            ChipGroupMsg::OnClickOverflowChip => {
+                self.is_open = !self.is_open;
+
+                ctx.props().on_overflow_chip_click.emit(());
+
+                true
+            }
+        }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html
@@ -41,14 +105,24 @@ impl Component for ChipGroup
             <div
                 class={classes!(
                     "pf-v5-c-chip-group",
+                    ctx.props().classes.clone(),
                     if ctx.props().category_name.is_empty() {""} else {"pf-m-category"},
                 )}
+                role="group"
+                // {...(categoryName && { 'aria-labelledby': id })}
+                // {...(!categoryName && { 'aria-label': ariaLabel })}
+                // {...getOUIAProps(ChipGroup.displayName, ouiaId)}
             >
                 <div class={"pf-v5-c-chip-group__main"}>
                     { &ctx.props().category_name }
-                    <ul class={"pf-v5-c-chip-group__list"} role={"list"}>
+                    <ul 
+                        class={"pf-v5-c-chip-group__list"}
+                        // {...(categoryName && { 'aria-labelledby': id })}
+                        // {...(!categoryName && { 'aria-label': ariaLabel })}
+                        role={"list"}
+                    >
                     {
-                        for ctx.props().children.iter().map(|child|
+                        for ctx.props().children.iter().enumerate().filter(|(index, _)| self.is_open || index < &(ctx.props().num_chips as usize)).map(|(_, child)|
                             html!{
                                 <li class={"pf-v5-c-chip-group__list-item"}>
                                 {
@@ -57,6 +131,33 @@ impl Component for ChipGroup
                                 </li>
                             }
                         )
+                    }
+                    {
+                        if ctx.props().children.len() as i32 > ctx.props().num_chips
+                        {
+                            html!{
+                                <li class={"pf-v5-c-chip-group__list-item"}>
+                                    <Chip
+                                        is_overflow_chip=true
+                                        onclick={ctx.link().callback(move |_| ChipGroupMsg::OnClickOverflowChip)}
+                                        component="button"
+                                    >
+                                    {
+                                        if self.is_open {
+                                            ctx.props().expanded_text.clone().unwrap_or(String::new())
+                                        } else {
+                                            let remaining = ctx.props().children.len() as i32 - ctx.props().num_chips;
+                                            ctx.props().collapsed_text.clone().unwrap_or(format!("{} more", remaining))
+                                        }
+                                    }
+                                    </Chip>
+                                </li>
+                            }
+                        }
+                        else
+                        {
+                            html!{}
+                        }
                     }
                     </ul>
                 </div>
@@ -68,6 +169,10 @@ impl Component for ChipGroup
                                 <Button
                                     variant={ButtonVariant::Plain}
                                     onclick={ctx.props().onclick.clone()}
+                                    aria_label={ctx.props().close_btn_aria_label.clone()}
+                                    // id={`remove_group_${id}`}
+                                    // aria-labelledby={`remove_group_${id} ${id}`}
+                                    // ouiaId={ouiaId || closeBtnAriaLabel}
                                 >
                                     <i class={"fas fa-times"}></i>
                                 </Button>
