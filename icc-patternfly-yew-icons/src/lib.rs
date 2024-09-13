aa9aeee0911c72bea3846989e120756aeb3bd3fd
Patrick Luell <pluell@iccdesigns.com>
2024-09-13 11:39:16 -0500

    icons: add arrow icons for sorting

--- icc-patternfly-yew-icons/src/lib.rs
+++ icc-patternfly-yew-icons/src/lib.rs
@@ -46,6 +46,17 @@ macro_rules! angle_up_icon {
 }
 
 #[macro_export]
+macro_rules! arrows_alt_v_icon {
+    () => {
+        html!{
+            <svg class="pf-v5-svg" viewBox="0 0 256 512" fill="currentColor" aria-hidden="true" role="img" width="1em" height="1em">
+                <path d="M214.059 377.941H168V134.059h46.059c21.382 0 32.09-25.851 16.971-40.971L144.971 7.029c-9.373-9.373-24.568-9.373-33.941 0L24.971 93.088c-15.119 15.119-4.411 40.971 16.971 40.971H88v243.882H41.941c-21.382 0-32.09 25.851-16.971 40.971l86.059 86.059c9.373 9.373 24.568 9.373 33.941 0l86.059-86.059c15.12-15.119 4.412-40.971-16.97-40.971z" />
+            </svg>
+        }
+    };
+}
+
+#[macro_export]
 macro_rules! bell_icon {
     () => {
         html!{
@@ -98,6 +109,28 @@ macro_rules! info_circle_icon {
             </svg>
         }
     }
+}
+
+#[macro_export]
+macro_rules! long_arrow_alt_down_icon {
+    () => {
+        html!{
+            <svg class="pf-v5-svg" viewBox="0 0 256 512" fill="currentColor" aria-hidden="true" role="img" width="1em" height="1em">
+                <path d="M168 345.941V44c0-6.627-5.373-12-12-12h-56c-6.627 0-12 5.373-12 12v301.941H41.941c-21.382 0-32.09 25.851-16.971 40.971l86.059 86.059c9.373 9.373 24.569 9.373 33.941 0l86.059-86.059c15.119-15.119 4.411-40.971-16.971-40.971H168z" />
+            </svg>
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! long_arrow_alt_up_icon {
+    () => {
+        html!{
+            <svg class="pf-v5-svg" viewBox="0 0 256 512" fill="currentColor" aria-hidden="true" role="img" width="1em" height="1em">
+                <path d="M88 166.059V468c0 6.627 5.373 12 12 12h56c6.627 0 12-5.373 12-12V166.059h46.059c21.382 0 32.09-25.851 16.971-40.971l-86.059-86.059c-9.373-9.373-24.569-9.373-33.941 0l-86.059 86.059c-15.119 15.119-4.411 40.971 16.971 40.971H88z" />
+            </svg>
+        }
+    };
 }
 
 #[macro_export]
