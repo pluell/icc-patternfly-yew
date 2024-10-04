b1444bd8d93252b20553ee4d8917225b602fbe20
Patrick Luell <pluell@iccdesigns.com>
2024-10-04 16:06:27 -0500

    PageContext: remove unused code

--- icc-patternfly-yew-core/src/components/page/page_context.rs
+++ icc-patternfly-yew-core/src/components/page/page_context.rs
@@ -13,13 +13,3 @@ pub struct PageContext
 //   getBreakpoint: (width: number | null) => 'default' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
 //   getVerticalBreakpoint: (height: number | null) => 'default' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
 }
-
-// export const pageContextDefaults: PageContextProps = {
-//   isManagedSidebar: false,
-//   isSidebarOpen: false,
-//   onSidebarToggle: () => null,
-//   width: null,
-//   height: null,
-//   getBreakpoint,
-//   getVerticalBreakpoint
-// };
