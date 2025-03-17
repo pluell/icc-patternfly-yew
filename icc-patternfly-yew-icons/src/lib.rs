a024612a16d49f5ee6fe64cd13c143a941b9dd91
Patrick Luell <pluell@iccdesigns.com>
2025-03-17 14:24:51 -0500

    icons: add minus icon

--- icc-patternfly-yew-icons/src/lib.rs
+++ icc-patternfly-yew-icons/src/lib.rs
@@ -156,6 +156,17 @@ macro_rules! long_arrow_alt_up_icon {
 }
 
 #[macro_export]
+macro_rules! minus_icon {
+    () => {
+        html!{
+            <svg class="pf-v5-svg" viewBox="0 0 448 512" fill="currentColor" aria-hidden="true" role="img" width="1em" height="1em">
+                <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+            </svg>
+        }
+    };
+}
+
+#[macro_export]
 macro_rules! outlined_question_circle_icon {
     () => {
         html!{
