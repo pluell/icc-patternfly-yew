c53fe94c6c4bd40571cc4064f1ef7d08dacc2034
Patrick Luell <pluell@iccdesigns.com>
2025-09-03 15:11:20 -0500

    ToggleGroup: add are_all_groups_disabled property

--- icc-patternfly-yew-core/src/components/toggle_group/toggle_group.rs
+++ icc-patternfly-yew-core/src/components/toggle_group/toggle_group.rs
@@ -1,5 +1,7 @@
 use yew::prelude::*;
 
+use crate::ToggleGroupItem;
+
 
 pub struct ToggleGroup;
 
@@ -8,16 +10,19 @@ pub struct ToggleGroupProps
 {
     /** Content rendered inside the toggle group */
     #[prop_or_default]
-    pub children: Children,
+    pub children: ChildrenWithProps<ToggleGroupItem>,
     /** Additional classes added to the toggle group */
     #[prop_or_default]
-    pub class_name: String,
+    pub classes: Classes,
     /** Modifies the toggle group to include compact styling. */
     #[prop_or_default]
     pub is_compact: bool,
+    /** Disable all toggle group items under this component. */
+    #[prop_or_default]
+    pub are_all_groups_disabled: bool,
     /** Accessible label for the toggle group */
     #[prop_or_default]
-    pub aria_label: String,
+    pub aria_label: AttrValue,
 }
 
 impl Component for ToggleGroup
@@ -37,13 +42,24 @@ impl Component for ToggleGroup
                 class={classes!(
                     "pf-v5-c-toggle-group", 
                     if ctx.props().is_compact { "pf-m-compact" } else { "" },
-                    ctx.props().class_name.clone()
+                    ctx.props().classes.clone()
                 )}
                 role="group"
                 aria-label={ctx.props().aria_label.clone()}
                 // {...props}
             >
-                { for ctx.props().children.iter() }
+            { 
+                for ctx.props().children.iter().map(|mut group_item| {
+                    let props = std::rc::Rc::make_mut(&mut group_item.props);
+
+                    // Mark the children as disabled if all groups are disabled
+                    if ctx.props().are_all_groups_disabled {
+                        props.is_disabled = true;
+                    }
+
+                    group_item
+                }) 
+            }
             </div>
         }
     }
